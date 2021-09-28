ef076055916cd52bad41436ae1a0d6e1a9e28c18
Rodrigo Campos <rodrigo.cdpadua@gmail.com>
2021-09-28 14:18:30 -0300

    simple and complex jsx element

---
+++ extension_jsx.jsx
@@ -0,0 +1,14 @@
+// Simple JSX Element
+const JSX = <h1>Hello JSX!</h1>;
+
+// Complex JSX Element
+const JSX = 
+<div>
+  <h1>Title</h1>
+  <p>Paragraph</p>
+  <ul>
+    <li>x</li>
+    <li>y</li>
+    <li>z</li>
+  </ul>
+</div>;
