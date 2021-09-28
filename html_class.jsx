32d433775287a3e793751cfc26095f8d27c3c4e3
Rodrigo Campos <rodrigo.cdpadua@gmail.com>
2021-09-28 14:42:33 -0300

    Define an HTML class in JSX

---
+++ html_class.jsx
@@ -0,0 +1,12 @@
+/*
+One key difference in JSX is that you can no longer use the word class to define HTML classes.
+This is because class is a reserved word in JavaScript. Instead, JSX uses className.
+*/
+const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
+/*
+The naming convention for all HTML attributes and event references in JSX become camelCase.
+*/
